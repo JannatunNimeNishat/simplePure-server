9ac7839e4f6b9cfbfafd3d53675cb9bd6e54f076
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-12 19:01:38 +0600

    api - get a specific servers api added

--- index.js
+++ index.js
@@ -13,53 +13,63 @@ require('dotenv').config()
 
 //mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oth2isl.mongodb.net/?retryWrites=true&w=majority`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    //
-    const servicesCollection = client.db('smilePureDB').collection('services')
-
-
-    //CRUD
-    
-    //read
-    app.get('/services', async(req,res)=>{
-
-      /*   const option = { 
-            projection: {service_name:1,img:1,details:1 }
-        } */
-        const cursors =  servicesCollection.find()
-        const result = await cursors.toArray();
-        console.log(result);
-        res.send(result);
-    })
+        //
+        const servicesCollection = client.db('smilePureDB').collection('services')
 
 
+        //CRUD
+
+        //read
+        app.get('/services', async (req, res) => {
+
+            /*   const option = { 
+                  projection: {service_name:1,img:1,details:1 }
+              } */
+            const cursors = servicesCollection.find()
+            const result = await cursors.toArray();
+            console.log(result);
+            res.send(result);
+        })
 
 
+        //read specific service 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const option = {
+                projection: {doctor_name: 1,doctor_img: 1, details: 1,price: 1, service_name:1}
+            }
+            const result = await servicesCollection.findOne(query,option)
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -67,12 +77,12 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('simplePure server is running')
 })
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`smilePure running at port: ${port}`);
 })
